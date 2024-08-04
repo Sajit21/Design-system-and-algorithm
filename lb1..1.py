3ebb77fbecac0b72bc830ad2d68d4da35103b3b7
Sajit Maharjan <126838978+Sajit21@users.noreply.github.com>
2024-08-04 19:41:55 +0545

    Update lb1..1.py

--- lb1..1.py
+++ lb1..1.py
@@ -1,54 +1,51 @@
-# //some changews have to be done so the chart can be done in increasing order
-
-def bubblesort(a,n):
- steps=0
- for i in range(1,n):
-    for j in range(n-1):
-       if(b[j+1]<b[j]):
-          temp=b[j+1]
-          b[j+1]=b[j]
-          b[j]=temp
+lab 1 def bubblesort(a,n):
+  steps=0
+  for i in range(1,n):#no of process
+    for j in range (n-i):
+      if(a[j+1]<a[j]):
+          temp=a[j+1]
+          a[j+1]=a[j]
+          a[j]=temp
           steps=steps+6
-          steps=steps+3
-          steps=steps+2
-          print("no of steps(bubble)",steps)
+        # The following line was unindented, causing the error.
+      steps=steps+3 #This line should be inside the inner for loop to count steps correctly
+    steps=steps+2
+  bsteps.append(steps)
+  print("No of steps(bubble):",steps)
+
 def selectionsort(b,n):
- steps=0
- for i in range(1,n):
-    min=b[i-1]
-    mi=i-1
-    steps=steps+4
-    for j in range(i,n):
-          if(b[j]<min):
-             min=b[j]
-             mi=j
-             steps=steps+5
-             steps=steps+3
-             temp=b[i-1]
-             b[i-1]=b[mi]
-             b[mi]=temp
-             steps=steps+3
-          print("no of steps(selection)",steps)
+    steps=0
+    for i in range (1,n):#no of process
+      min=b[i-1]
+      mi=i-1
+      steps=steps+4
+      for j in range(i,n):#finds min
+        if(b[j]<min):
+          min=b[j]
+          mi=j;
+          steps=steps+5
+        steps=steps+3
+      temp=b[i-1]
+      b[i-1]=b[mi]
+      b[mi]=temp
+      steps=steps+3
+    ssteps.append(steps)
+    print("No of steps(Selection):",steps)
 
+#driven code
 
- 
-
-         
-
-
-
-
-#driver code
 import random
-n= int(input("enter n number:"))
-a=[]
-b=[]
-for i in range(n):
+bsteps=[]
+ssteps=[]
+for j in range(0,10):
+  n=int(input("Enter n:"))
+  a= []
+  b= []
+  for i in range(n):
     e=random.randint(0,n)
     a.append(e)
     b.append(e)
-
-bubblesort(a,n)
-print("sorted data bubble",a)
-selectionsort(b,n)
-print("sorted data selection ", b)       
+  bubblesort(a,n)
+  print("Sorted data(bubble):",a)
+  selectionsort(b,n)
+  print("Sorted data(Selection):",b)
